0563e33c9ddf13251780b45a7e17fb98cb117bec
Jakaria Rabbi <Jakaria.Rabbi@aer.ca>
2019-06-03 17:16:45 -0600

    keras frcnn

---
+++ testing_code.py
@@ -0,0 +1,6 @@
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout
+def test():
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    print('hello')
